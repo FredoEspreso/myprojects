6fdb4fdffcb65f67ac093f5fbf8cba81a04661ac
FredoEspreso <giannislykourinas@gmail.com>
2022-09-18 19:45:09 +0300

    modified:   bird_simulation.py 	deleted:    bird_simulation_test.py

--- bird_simulation.py
+++ bird_simulation.py
@@ -10,10 +10,10 @@ from grid_data_structure import Grid
 
 imgs_folder="\\imgs\\"
 WIDTH=1200
-HEIGHT=1080
-noofagents=70
+HEIGHT=1000
+noofagents=200
 fps=30
-cell_size=120
+cell_size=40
 
 
 class Simulation:
@@ -42,16 +42,16 @@ class Simulation:
 	def create_agents(self):
 		self.agents=[]
 		for i in range(noofagents):
-			agent=Bird((random.randint(30,WIDTH-1-30),random.randint(30,HEIGHT-1-30)),
+			agent=Bird(cell_size,(random.randint(30,WIDTH-1-30),random.randint(30,HEIGHT-1-30)),
 								0,
 								"arrow.png")
 			self.agents.append(agent)
 
 	#updates the state of the agents
 	def update(self):
-		#grid=Grid(self.agents,cell_size,WIDTH,HEIGHT)
+		grid=Grid(self.agents,cell_size,WIDTH,HEIGHT)
 		for agent in self.agents:
-			agent.update(self.agents)
+			agent.update(grid)
 
 	#draw graphics
 	def draw_window(self,win):
@@ -65,15 +65,20 @@ class Simulation:
 	def del_and_spawn(self):
 		idx_to_delete=[]
 		for i,agent in enumerate(self.agents):
-			if(np.linalg.norm(agent.pos-(WIDTH/2,HEIGHT/2))>800):
+			#if he's outta bounds he gotta go
+			if not (0<=agent.pos[0]<=WIDTH and 0<=agent.pos[1]<=HEIGHT):
 				idx_to_delete=[i]+idx_to_delete
+			#track em down and pop em
 		for i in idx_to_delete:
 			self.agents.pop(i)
+			#add as many as we deleted
 		for i in range(len(idx_to_delete)):
-			self.agents.append(Bird((random.randint(30,WIDTH-1-30),random.randint(30,HEIGHT-1-30)),
+			self.agents.append(Bird(cell_size,(random.randint(30,WIDTH-1-30),random.randint(30,HEIGHT-1-30)),
 								0,
 								"arrow.png"))
 
 if (__name__=="__main__"):
 	s=Simulation()
+	grid=Grid(s.agents,cell_size,WIDTH,HEIGHT)
+	print(grid.getNeighbours(s.agents[0]))
 	s.start()
